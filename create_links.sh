13cda3d1e1dd7f77452972894f99b36e2e186fbf
Mike Hearn <git@mikehearn.net>
2016-09-12 22:26:19 +0000

    Added simple npm config

--- create_links.sh
+++ create_links.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env /bin/bash
 
-# Vim
-ln -sf $(pwd)/vim/vimrc $HOME/.vimrc
-
 # Bash
 ln -sf $(pwd)/bash/bash_aliases $HOME/.bash_aliases
 ln -sf $(pwd)/bash/bash_profile $HOME/.profile
 
+# Node
+ln -sf $(pwd)/node/npmrc $HOME/.npmrc
+
 # Tmux
 ln -sf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
+
+# Vim
+ln -sf $(pwd)/vim/vimrc $HOME/.vimrc
 

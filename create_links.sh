94d3be9619cdf5eb1b97edcf7475d21d37ccee63
Michael Hearn <git@mikehearn.net>
2016-10-12 09:36:52 -0400

    Added git config

--- create_links.sh
+++ create_links.sh
@@ -13,3 +13,7 @@ ln -sf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
 # Vim
 ln -sf $(pwd)/vim/vimrc $HOME/.vimrc
 
+# Git
+ln -sf $(pwd)/git/gitconfig $HOME/.gitconfig
+ln -sf $(pwd)/git/gitignore_global $HOME/.gitignore_global
+

d641b6d72b81d1153333885529a8f244c88715b4
Michael Hearn <git@mikehearn.net>
2016-10-17 00:56:17 +0000

    Added global stylelintrc

--- create_links.sh
+++ create_links.sh
@@ -8,6 +8,7 @@ ln -sf $(pwd)/bash/bash_profile $HOME/.profile
 ln -sf $(pwd)/node/npmrc $HOME/.npmrc
 ln -sf $(pwd)/node/tern-project $HOME/.tern-project
 ln -sf $(pwd)/node/eslintrc $HOME/.eslintrc
+ln -sf $(pwd)/node/stylelintrc $HOME/.stylelintrc
 
 # Tmux
 ln -sf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
@@ -15,7 +16,7 @@ ln -sf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
 # Vim
 mkdir -p $HOME/.vim
 ln -sf $(pwd)/vim/vimrc $HOME/.vimrc
-ln -sf $(pwd)/vim/UltiSnips $HOME/.vim/UltiSnips
+ln -sf $(pwd)/vim/UltiSnips $HOME/.vim/
 
 # Git
 ln -sf $(pwd)/git/gitconfig $HOME/.gitconfig

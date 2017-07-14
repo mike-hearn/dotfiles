315786d52da0d13d12a61e06e7e6c7dc21c713b0
Michael Hearn <git@mikehearn.net>
2017-07-14 15:56:00 -0400

    Rename create_links to init

--- init.sh
+++ init.sh
@@ -10,15 +10,22 @@ ln -sf $(pwd)/node/eslintrc $HOME/.eslintrc
 ln -sf $(pwd)/node/stylelintrc $HOME/.stylelintrc
 
 # Tmux
-ln -sf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
-mkdir -p ~/.tmux/plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d $HOME/.tmux/plugins ]; then
+    mkdir -p $HOME/.tmux/plugins
+    ln -sf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
 # Vim
 mkdir -p $HOME/.vim
 ln -sf $(pwd)/vim/vimrc $HOME/.vimrc
 ln -sf $(pwd)/vim/UltiSnips $HOME/.vim/
 ln -sf $(pwd)/vim/UltiSnips $HOME/.config/nvim/
+
+# Neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -sf $(pwd)/nvim/init.vim $HOME/.config/nvim/init.vim
 
 # Git
 ln -sf $(pwd)/git/gitconfig $HOME/.gitconfig

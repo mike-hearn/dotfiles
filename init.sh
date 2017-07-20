590f368c8807cf2268a417d6b457017055ebe197
Michael Hearn <git@mikehearn.net>
2017-07-20 08:22:45 -0400

    Update init.sh for nvim/base16

--- init.sh
+++ init.sh
@@ -3,6 +3,9 @@
 # Bash
 ln -sf $(pwd)/bash/bash_profile $HOME/.profile
 
+# Bash colorscheme
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell 2> /dev/null
+
 # Node
 ln -sf $(pwd)/node/npmrc $HOME/.npmrc
 ln -sf $(pwd)/node/tern-project $HOME/.tern-project
@@ -10,9 +13,9 @@ ln -sf $(pwd)/node/eslintrc $HOME/.eslintrc
 ln -sf $(pwd)/node/stylelintrc $HOME/.stylelintrc
 
 # Tmux
+ln -sf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
 if [ ! -d $HOME/.tmux/plugins ]; then
     mkdir -p $HOME/.tmux/plugins
-    ln -sf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
@@ -23,9 +26,11 @@ ln -sf $(pwd)/vim/UltiSnips $HOME/.vim/
 ln -sf $(pwd)/vim/UltiSnips $HOME/.config/nvim/
 
 # Neovim
+mkdir -p $HOME/.config/nvim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -sf $(pwd)/nvim/init.vim $HOME/.config/nvim/init.vim
+ln -sf $(pwd)/nvim/functions.vim $HOME/.config/nvim/functions.vim
 
 # Editorconfig
 ln -sf $(pwd)/editorconfig/editorconfig $HOME/.editorconfig

37ff82d23af9980d8f4de628040ed414af6dd197
Michael Hearn <git@mikehearn.net>
2018-07-23 10:38:29 -0400

    Remove nvim/tmux from init script

--- init.sh
+++ init.sh
@@ -34,15 +34,6 @@ ln -sf $(pwd)/node/tern-project $HOME/.tern-project
 ln -sf $(pwd)/node/eslintrc $HOME/.eslintrc
 ln -sf $(pwd)/node/stylelintrc $HOME/.stylelintrc
 
-# Tmux
-if [ ! -d $HOME/.tmux/plugins ]; then
-    mkdir -p $HOME/.tmux/plugins
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
-ln -sf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
-ln -sf $(pwd)/tmux/restartsession.sh $HOME/.tmux/restartsession.sh
-ln -sf $(pwd)/tmux/choosesession.sh $HOME/.tmux/choosesession.sh
-
 # Tmuxinator
 lnd $(pwd)/tmuxinator $HOME/.tmuxinator
 
@@ -52,13 +43,6 @@ mkdir -p $HOME/.vim/tmp/undo $HOME/.vim/tmp/backup $HOME/.vim/tmp/swap
 ln -sf $(pwd)/vim/vimrc $HOME/.vimrc
 ln -sf $(pwd)/vim/UltiSnips $HOME/.vim/
 ln -sf $(pwd)/vim/UltiSnips $HOME/.config/nvim/
-
-# Neovim
-mkdir -p $HOME/.config/nvim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -sf $(pwd)/nvim/init.vim $HOME/.config/nvim/init.vim
-ln -sf $(pwd)/nvim/functions.vim $HOME/.config/nvim/functions.vim
 
 # Editorconfig
 ln -sf $(pwd)/editorconfig/editorconfig $HOME/.editorconfig

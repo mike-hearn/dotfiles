3dd37aeb0541351a7c8ebaa7b5561aa1d57fb4b9
Michael Hearn <git@mikehearn.net>
2019-08-12 21:16:55 -0400

    Only need one vscode IPC hook per machine

--- shell/.config/shell/zsh/autosource/set_vscode_socket.zsh
+++ shell/.config/shell/zsh/autosource/set_vscode_socket.zsh
@@ -6,7 +6,6 @@ if [[ -v VSCODE_IPC_HOOK_CLI ]]; then
 
 	if [[ -v gitroot ]]; then
 		echo $(which code) > ~/.config/vscode/code_binary_location
-		sed -i "/^$gitroot=/d" ~/.config/vscode/vscode_ipc_hook_map
-		echo "$gitroot=$VSCODE_IPC_HOOK_CLI" >> ~/.config/vscode/vscode_ipc_hook_map
+		echo $VSCODE_IPC_HOOK_CLI > ~/.config/vscode/vscode_ipc_hook_map
 	fi
 fi

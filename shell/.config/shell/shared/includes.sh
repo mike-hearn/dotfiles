946bcc1eca34311268a1598932e7b9d3109729b6
Michael Hearn <git@mikehearn.net>
2020-04-02 15:58:11 -0400

    Source .env for env specific stuff

--- shell/.config/shell/shared/includes.sh
+++ shell/.config/shell/shared/includes.sh
@@ -2,6 +2,7 @@
 
 # environment specific settings & secrets
 [ -f $HOME/.bash_env ] && . $HOME/.bash_env
+[ -f $HOME/.env ] && . $HOME/.env
 [ -f $HOME/.secrets ] && . $HOME/.secrets
 [ -f /etc/profile.d/autojump.sh ] && . /etc/profile.d/autojump.sh
 

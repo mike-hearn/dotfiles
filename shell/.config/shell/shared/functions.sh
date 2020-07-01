fdbf8de692929ca659ef33498fc0ae6a7349daa7
Michael Hearn <git@mikehearn.net>
2020-07-01 13:43:55 -0400

    Add global deactivate function for venvs

--- shell/.config/shell/shared/functions.sh
+++ shell/.config/shell/shared/functions.sh
@@ -455,4 +455,35 @@ fshow_preview() {
         --bind "alt-y:execute:$_gitLogLineToHash | xclip"
     }
 
+deactivate () {
+    if [ -n "${_OLD_VIRTUAL_PATH:-}" ]
+    then
+        PATH="${_OLD_VIRTUAL_PATH:-}"
+        export PATH
+        unset _OLD_VIRTUAL_PATH
+    fi
+    if [ -n "${_OLD_VIRTUAL_PYTHONHOME:-}" ]
+    then
+        PYTHONHOME="${_OLD_VIRTUAL_PYTHONHOME:-}"
+        export PYTHONHOME
+        unset _OLD_VIRTUAL_PYTHONHOME
+    fi
+    if [ -n "${BASH:-}" -o -n "${ZSH_VERSION:-}" ]
+    then
+        hash -r
+    fi
+    if [ -n "${_OLD_VIRTUAL_PS1:-}" ]
+    then
+        PS1="${_OLD_VIRTUAL_PS1:-}"
+        export PS1
+        unset _OLD_VIRTUAL_PS1
+    fi
+    unset VIRTUAL_ENV
+    if [ ! "$1" = "nondestructive" ]
+    then
+        unset -f deactivate
+    fi
+}
+
+
 # vim: syntax=sh ts=4 sts=4 shiftwidth=4 expandtab

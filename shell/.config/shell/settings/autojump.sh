e30e3cd7b3b0507b46f763251a961d1f1dd34533
Michael Hearn <git@mikehearn.net>
2020-07-14 16:02:01 -0400

    We're a z shop now

--- shell/.config/shell/settings/autojump.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Autojump
-[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
-[ -f /etc/profile.d/autojump.sh ] && . /etc/profile.d/autojump.sh

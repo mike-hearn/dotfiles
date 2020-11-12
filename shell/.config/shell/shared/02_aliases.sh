9816f813a088dc66d12f2b689aa4bcd48ad34ff0
Michael Hearn <git@mikehearn.net>
2020-11-12 11:48:56 -0500

    Sort aliases

--- shell/.config/shell/shared/02_aliases.sh
+++ shell/.config/shell/shared/02_aliases.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-# Git
+
+# Simple Alises
+
+alias c='clear'
+alias gronfzf='gron -c | fzf --ansi'  # pipe json into gron + fzf filtering
 alias ga="git add " alias gaa="git add --all"
 alias gb="git branch "
 alias gbr="git for-each-ref --sort=-committerdate refs/remotes/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
@@ -16,20 +20,18 @@ alias gps="git stash save --include-untracked; git pull; git stash pop;"
 alias grb="git for-each-ref --sort=-committerdate refs/remotes/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
 alias gs="echo;echo;git status -s;echo;"
 alias gx="gitx --all"
+alias lg="lazygit"
+alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias pull="git pull"
 alias recent="git for-each-ref --sort=-committerdate"
 alias recentdate="git for-each-ref --sort=-committerdate refs/heads/ --format='%(refname) %(committerdate) %(authorname)' | sed 's/refs\/heads\///g'"
-alias lg="lazygit"
-
-# Read things aloud faster
+alias rg="rg --max-columns=1000 --smart-case"
 alias say="say --voice=Daniel --rate=275"
-
-# Quick commands
-alias c='clear'
+alias systembin="export PATH=/usr/bin:$PATH"  # move the system bin path to the front of $PATH
 alias x='exit'
 
-# Find external IP address
-alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
+# Computed Aliases
+
 # ls priority: exa, then gls, then ls
 if [ -x "$(command -v exa)" ]; then
     alias ls="command exa -l --group-directories-first --git"
@@ -38,12 +40,3 @@ if [ -x "$(command -v exa)" ]; then
 else
     alias ls="command ls -la"
 fi
-
-# Default ripgrep settings
-alias rg="rg --max-columns=1000 --smart-case"
-
-# autojump key now uses z, in case I can't fix my muscle memory
-alias j="z"
-
-# move the system bin path to the front of $PATH
-alias systembin="export PATH=/usr/bin:$PATH"

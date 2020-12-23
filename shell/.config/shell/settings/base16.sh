86957d19d594bb85713b0d663f2e563558cfdf3b
Michael Hearn <git@mikehearn.net>
2020-12-23 14:41:28 -0500

    Going back to less clever base16 import

--- shell/.config/shell/settings/base16.sh
+++ shell/.config/shell/settings/base16.sh
@@ -2,21 +2,18 @@
 
 BASE16_SHELL="$HOME/.config/base16-shell/"
 
-function _load_base16 {
-    [ -n "$PS1" ] && \
-        [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
-        "$BASE16_SHELL/profile_helper.sh" | head -n 2 > ~/.base16_bootstrap
-}
-
 function base16_bootstrap {
     [ -n "$PS1" ] && \
         [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
         eval "$("$BASE16_SHELL/profile_helper.sh")"
 }
 
-( _load_base16 & )
+function _load_base16 {
+    base16_bootstrap > ~/.base16_bootstrap
+}
 
-. ~/.base16_bootstrap
+
+base16_bootstrap
 
 # vim: syntax=sh ts=4 sts=4 shiftwidth=4 expandtab
 

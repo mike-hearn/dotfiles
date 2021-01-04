f171b2a747ff38ac846360e1a9ef3d719b9abd60
Michael Hearn <git@mikehearn.net>
2021-01-04 11:52:37 -0500

    Update tmux choosesession script

--- tmux/.tmux/choosesession.sh
+++ tmux/.tmux/choosesession.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
 
-TMUXSESSIONS=$(tmux list-sessions -F "#{session_name}")
-TMUXINATORSESSIONS=$(\ls ~/.tmuxinator/ | sed -e 's/\.yml$//' | sed -e 's/[ -_]/-/g')
-
-function tmux_switch_to_session() {
-	read session
-
-	if   [[ $TMUXSESSIONS =~ .*$session.* ]]; then
-		tmux switch-client -t "$session"
-	elif [[ $TMUXINATORSESSIONS =~ .*$session.* ]]; then
-		tmuxinator $(echo $session | sed -e 's/[-_ ]/-/')
-	fi
-}
-
-cat <(echo "$TMUXSESSIONS") <(echo "$TMUXINATORSESSIONS") \
-	| sort -rfu \
-	| fzf-tmux \
-	| tr -d '\n' \
-	| tmux_switch_to_session
+[[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
+session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf-tmux --exit-0) &&  tmux $change -t "$session" || exit 0
